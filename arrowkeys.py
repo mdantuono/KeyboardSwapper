a497e228e08d90763a8063645ab6482563814b45
mdantuono <mikeishere3@gmail.com>
2024-10-18 11:47:59 -0400

    first commit

---
+++ arrowkeys.py
@@ -0,0 +1,67 @@
+import tkinter as tk
+import keyboard
+
+normalText = "Keys are normal"
+activeText = "Keys are swapped to arrows"
+
+state = False
+
+def buttonClicked():
+    global state
+
+    print("Button clicked!")
+    if state is False:
+        swapKeys()
+        state = True
+        label.config(text=activeText)
+
+
+    elif state is True:
+        unswap()
+        state = False
+        label.config(text=normalText)
+
+    label.pack(padx=20, pady=20)
+
+def swapKeys():
+    keyboard.remap_key('j', 'left')
+    keyboard.remap_key('l', 'right')
+    keyboard.remap_key('i', 'up')
+    keyboard.remap_key('k', 'down')
+
+def unswap():
+    keyboard.unhook_all()
+
+root = tk.Tk()
+
+# Creating a button with specified options
+button = tk.Button(root, 
+                   text="Swap", 
+                   command=buttonClicked,
+                   activebackground="blue", 
+                   activeforeground="white",
+                   anchor="center",
+                   bd=3,
+                   bg="lightgray",
+                   cursor="hand2",
+                   disabledforeground="gray",
+                   fg="black",
+                   font=("Arial", 12),
+                   height=2,
+                   highlightbackground="black",
+                   highlightcolor="green",
+                   highlightthickness=2,
+                   justify="center",
+                   overrelief="raised",
+                   padx=10,
+                   pady=5,
+                   width=15,
+                   wraplength=100)
+
+label = tk.Label(text="Click to swap keys")
+
+button.pack(padx=20, pady=20)
+label.pack(padx=20, pady=20)
+
+
+root.mainloop()
---
+++ keys.cmd
@@ -0,0 +1,1 @@
+python C:\Users\Mike\Documents\Projects\keys\arrowkeys.py
